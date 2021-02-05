095498074592c7b62affff8e7231453bc74c95b6
Yeraldina <yestrella14@gmail.com>
2021-02-04 23:36:18 -0500

    First commit

---
+++ my_package/__init__.py
@@ -0,0 +1,5 @@
+#add and import the modules here
+#from df_utils import *
+__version__ = 0.0.1
+my_str = "This is my String from __init__.py"
+
---
+++ my_package/df_utils.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+
+ONES = pd.Series(np.ones(10))
+ZEROS = pd.Series(np.zeros(5))
